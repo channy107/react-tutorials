6acbd60d88dfbccfee8d5e7ed94c86f5cd902aaf
user <hunchan12@gmail.com>
2019-05-14 10:42:57 +0900

    Redux store setting

--- src/index.js
+++ src/index.js
@@ -2,6 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import App from './components/App';
+import { createStore } from 'redux';
+import reducers from './reducers';
+
+//import * as actions from './actions';
+
+const store = createStore(reducers);
+
+// console.log(store.getState());
+// const unsubscribe = store.subscribe(() => console.log(store.getState()));
+// store.subscribe(() => console.log(store.getState()));
+// store.dispatch(actions.increment());
+// store.dispatch(actions.increment());
+// store.dispatch(actions.decrement());
+// store.dispatch(actions.setColor([200, 200, 200]));
+
+// // 이렇게 하면 더이상 console에 찍히지 않음
+// unsubscribe();
 
 ReactDOM.render(
     <App />,
